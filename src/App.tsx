6d51d41fa925e7b22be13d27b45cc04e88318bda
Cristhian Fernández <CristhianF7@gmail.com>
2022-08-18 14:47:29 -0500

    React routing and storybook implementation (#3)
    * feat: react routing and storybook implementation
    
    * feat: react redux implementation (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './containers/home';
 
 const App: FunctionComponent = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
